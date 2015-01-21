389e1e0a263f55e1449f0053e3693b5a872b849a
Lee Olayvar <leeolayvar@gmail.com>
2015-01-21 06:30:34 +0000

    Added Dest() Streamer

--- streamer_test.go
+++ streamer_test.go
@@ -1,6 +1,8 @@
 package muta
 
 import (
+	"io/ioutil"
+	"os"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -17,6 +19,8 @@ func TestSrcStreamer(t *testing.T) {
 		So(rb, ShouldResemble, b)
 		s(fi, b)
 	})
+
+	Convey("Should support globbing", t, nil)
 
 	Convey("Should load the given file and", t, func() {
 		Convey("Populate FileInfo with the file info", func() {
@@ -91,4 +95,81 @@ func TestSrcStreamer(t *testing.T) {
 			s(nil, nil)
 		})
 	})
+}
+
+func TestDest(t *testing.T) {
+	os.RemoveAll("_test/tmp/dest")
+
+	Convey("Should create the the destination if needed", t, func() {
+		s := Dest("_test/tmp/dest")
+		f := &FileInfo{
+			Name: "file",
+			Path: ".",
+		}
+		c := []byte("chunk")
+		s(f, c)
+		s(nil, nil)
+		osFi, err := os.Stat("_test/tmp/dest")
+		So(err, ShouldBeNil)
+		So(osFi.IsDir(), ShouldBeTrue)
+	})
+
+	os.RemoveAll("_test/tmp/path")
+
+	Convey("Should create the path in the dest if needed", t, func() {
+		s := Dest("_test/tmp")
+		f := &FileInfo{
+			Name: "file",
+			Path: "path/foo/bar",
+		}
+		c := []byte("chunk")
+		_, _, err := s(f, c)
+		So(err, ShouldBeNil)
+		osFi, err := os.Stat("_test/tmp/path/foo/bar")
+		So(err, ShouldBeNil)
+		So(osFi.IsDir(), ShouldBeTrue)
+	})
+
+	os.Remove("_test/tmp/file")
+
+	Convey("Should create the file in the destination", t, func() {
+		s := Dest("_test/tmp")
+		f := &FileInfo{
+			Name: "file",
+			Path: ".",
+		}
+		c := []byte("foo")
+		_, _, err := s(f, c)
+		So(err, ShouldBeNil)
+		// Signal EOF
+		_, _, err = s(nil, nil)
+		So(err, ShouldBeNil)
+		// Test the file
+		_, err = os.Stat("_test/tmp/file")
+		So(err, ShouldBeNil)
+	})
+
+	os.Remove("_test/tmp/file")
+
+	Convey("Should write incoming bytes to the given file", t, func() {
+		s := Dest("_test/tmp")
+		f := &FileInfo{
+			Name: "file",
+			Path: ".",
+		}
+		_, _, err := s(f, []byte("foo"))
+		So(err, ShouldBeNil)
+		_, _, err = s(f, []byte("bar"))
+		So(err, ShouldBeNil)
+		_, _, err = s(f, []byte("baz"))
+		So(err, ShouldBeNil)
+		_, _, err = s(nil, nil)
+		So(err, ShouldBeNil)
+
+		b, err := ioutil.ReadFile("_test/tmp/file")
+		So(err, ShouldBeNil)
+		So(b, ShouldResemble, []byte("foobarbaz"))
+	})
+
+	Convey("Should not allow writing outside of the destination", t, nil)
 }
